1185fa6360e4b0907546ae24711cdf7587c42cb0
CalumCannon <calbo90@googlemail.com>
2019-06-13 12:25:35 +0100

    adds stuff

--- src/main/java/com/codeclan/coursebooking/coursebookings/repositories/CourseRepository/CourseRepository.java
+++ src/main/java/com/codeclan/coursebooking/coursebookings/repositories/CourseRepository/CourseRepository.java
@@ -3,5 +3,10 @@ package com.codeclan.coursebooking.coursebookings.repositories.CourseRepository;
 import com.codeclan.coursebooking.coursebookings.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    public List<Course> findCoursesByStarRating(int rating);
+
 }
--- src/test/java/com/codeclan/coursebooking/coursebookings/CoursebookingsApplicationTests.java
+++ src/test/java/com/codeclan/coursebooking/coursebookings/CoursebookingsApplicationTests.java
@@ -1,7 +1,14 @@
 package com.codeclan.coursebooking.coursebookings;
 
+import com.codeclan.coursebooking.coursebookings.models.Booking;
+import com.codeclan.coursebooking.coursebookings.models.Course;
+import com.codeclan.coursebooking.coursebookings.models.Customer;
+import com.codeclan.coursebooking.coursebookings.repositories.BookingRepository.BookingRepository;
+import com.codeclan.coursebooking.coursebookings.repositories.CourseRepository.CourseRepository;
+import com.codeclan.coursebooking.coursebookings.repositories.CustomerRepository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +16,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CoursebookingsApplicationTests {
 
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testFindCourseByRating(){
+		Course course = new Course("CodeClan", "Glasgow", 5);
+		courseRepository.save(course);
+
+		Customer customer = new Customer("Calum", "Locwhinnoch", 26);
+		customerRepository.save(customer);
+
+		Booking booking1 = new Booking("13/06/2019", course, customer);
+		Booking booking2 = new Booking("14/06/2019", course, customer);
+		bookingRepository.save(booking1);
+		bookingRepository.save(booking2);
 	}
 
 }
